b50b35319346dd9d62cedd575d4a00a88aa538b8
danielr96 <35438790+danielr96@users.noreply.github.com>
2018-03-21 08:33:48 -0600

    Update unidad114161334.asm

--- unidad114161334.asm
+++ unidad114161334.asm
@@ -7,28 +7,28 @@ STDIN equ 0
 STDOUT equ 1
 
 segment .data 
-	mensaje db  0xA,0xD,"El resultado es: "
-	lengthMensaje equ $- mensaje
-	salto db  0xA,0xD
-	lengthSalto equ $- salto
+    mensaje db  0xA,0xD,"El resultado es: " 
+    lengthMensaje equ $- mensaje
+    salto db  0xA,0xD
+    lengthSalto equ $- salto
     lim_inf equ 0
     lim_sup equ 2
 
 segment .bss
-	res resb 1
+    res resb 1
     aux1 resb 1    
     aux2 resb 1
 
 section  .text
-	global _start
+    global _start
 _start:
     mov eax, SYS_PRINT 
-	mov ebx, STDOUT
-	mov ecx, mensaje
-	mov edx, lengthMensaje
-	int 0x80
+    mov ebx, STDOUT
+    mov ecx, mensaje
+    mov edx, lengthMensaje
+    int 0x80
 ; INICIO LIMITE SUPERIOR
-	;4(0)
+    ; 4(2)
     mov eax, 4
     mov ebx, lim_sup
     mul ebx
@@ -42,7 +42,7 @@ _start:
     mov ecx, 2
     div ecx
     mov ecx, eax; ecx = 6
-    ; -(2)³
+    ; (2)³
     mov eax, lim_sup
     mov ebx, lim_sup
     mul ebx
@@ -55,7 +55,7 @@ _start:
     mov [aux1], eax;
 ; FIN LIMITE SUPERIOR
 ; INICIO LIMITE INFERIOR
-	;4(0)
+    ; 4(0)
     mov eax, 4
     mov ebx, lim_inf
     mul ebx
@@ -69,7 +69,7 @@ _start:
     mov ecx, 2
     div ecx
     mov ecx, eax; ecx = 0
-    ; -(0)³
+    ; (0)³
     mov eax, lim_inf
     mov ebx, lim_inf
     mul ebx
@@ -84,19 +84,19 @@ _start:
     sub ebx, eax
 ; IMPRESION EN PANTALLA
     add ebx, 48
-	mov [res], ebx
-	mov eax, SYS_PRINT 
-	mov ebx, STDOUT
-	mov ecx, res 
-	mov edx, 1
-	int 0x80
+    mov [res], ebx
+    mov eax, SYS_PRINT 
+    mov ebx, STDOUT
+    mov ecx, res 
+    mov edx, 1
+    int 0x80
 ; IMPRESION EN PANTALLA
-	mov eax, SYS_PRINT 
-	mov ebx, STDOUT
-	mov ecx, salto 
-	mov edx, lengthSalto
-	int 0x80
+    mov eax, SYS_PRINT 
+    mov ebx, STDOUT
+    mov ecx, salto 
+    mov edx, lengthSalto
+    int 0x80
 salir:
-	mov eax, SYS_SALIDA  
-	xor ebx, ebx 
-	int 0x80
+    mov eax, SYS_SALIDA  
+    xor ebx, ebx 
+    int 0x80
